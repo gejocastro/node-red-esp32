23173a3d997199172ab3a9ad138503b747dc601e
gejocastro <gejocastro@gmail.com>
2022-05-15 20:23:05 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -121,14 +121,14 @@ module.exports = {
     // -----------------
     // To password protect the Node-RED editor and admin API, the following
     // property can be used. See http://nodered.org/docs/security.html for details.
-    adminAuth: {
-        type: "credentials",
-        users: [{
-           username: "user",
-            password: "$2b$08$0wzwQKnHOuCa.0jMtCFjqO.o2.HNgWRg94q7kxuY.9Rp.L7aBR8Hy",
-            permissions: "*"
-        }]
-    },
+//     adminAuth: {
+//         type: "credentials",
+//         users: [{
+//            username: "user",
+//             password: "$2b$08$0wzwQKnHOuCa.0jMtCFjqO.o2.HNgWRg94q7kxuY.9Rp.L7aBR8Hy",
+//             permissions: "*"
+//         }]
+//     },
 
     // To password protect the node-defined HTTP endpoints (httpNodeRoot), or
     // the static content (httpStatic), the following properties can be used.
